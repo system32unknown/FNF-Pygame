b80501f053062c0d7630da330674ef404f116d59
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2025-09-16 14:09:51 +0900

    strum stuff

--- obj/Strumline.py
+++ obj/Strumline.py
@@ -1,10 +1,34 @@
 from settings import *
 
-class StrumNote(pg.sprite.Sprite):
-    downscroll:bool = False
+class Strumline(pg.sprite.Group):
+    ai:bool = False
+    keyCount:int = 0
+
+    healthMult:float = 1
+    desiredWidth:float = 160
+
+    sizeMult:float = 0
+    spacing:float = 0
+
     def __init__(self):
         super().__init__()
 
-class Strumline(pg.sprite.Group):
-    def __init__(self, *sprites):
-        super().__init__(*sprites)
+    def regenerate(self):
+        self.empty()
+
+        for i in range(self.keyCount):
+
+            strum = Receptor(self, i)
+            self.add(strum)
+
+class Receptor(pg.sprite.Sprite):
+    parent:Strumline = None
+    queueStatic:bool = False
+    holdTime:float = 0.0
+
+    eRecepter:float = 0.0
+
+    def __init__(self, parent:Strumline, lane:int):
+        super().__init__()
+
+        self.parent = parent
